557b40aa35284e4acd006490b120dd8ad8774e90
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-26 10:46:04 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", formSubmit)
 });
+
+function formSubmit(event){
+  event.preventDefault();
+  let inputTask = document.getElementById("new-task-description");
+let inputTaskList = document.createElement("li")
+  inputTaskList.textContent = inputTask.value
+
+  let outputList = document.getElementById("tasks");
+  // outputList.append(inputTaskList);
+  outputList.appendChild(inputTaskList);
+event.target.reset()
+}
